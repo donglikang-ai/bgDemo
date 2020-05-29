89feb517d34d49d42fe02ead98bc32c8822b8618
donglikang@pinzhikeji.com <kang1203>
2020-05-29 18:33:06 +0800

    sentinelResource

--- sentinel-payment8401/src/main/java/com/cxb/bg/controller/SentinelPayment8401Controller.java
+++ sentinel-payment8401/src/main/java/com/cxb/bg/controller/SentinelPayment8401Controller.java
@@ -1,7 +1,11 @@
 package com.cxb.bg.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author dlkang
@@ -11,13 +15,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SentinelPayment8401Controller {
 
-    @GetMapping("test/a")
-    public String testA(){
+    @GetMapping("testA")
+    public String testA() {
         return "testA...";
     }
 
-    @GetMapping("test/b")
-    public String testB(){
-        return "testA...";
+    @GetMapping("testB")
+    public String testB() {
+        return "testB...";
+    }
+
+
+    @GetMapping("testC")
+    public String testC() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "testC...";
+    }
+
+
+
+    @GetMapping("testD")
+    @SentinelResource(value = "testD",blockHandler = "deal_testD")
+    public String testD() {
+
+        int a=10/0;
+
+        return "testD...";
+    }
+
+
+    public String deal_testD(BlockException exception){
+        return "(┬＿┬)   老哥，服务器出现故障";
     }
 }
